38f487039bf92f0631262829b7fc73d04dfc8281
Gromeous <gromeous@gmail.com>
2020-07-09 21:57:19 -0300

    assests

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ beersArr = [];
     getBeers()
 })();
 
+(()=>{
+    getAssets()
+})();
+
 function getBeers(){
     //consumir un json de modo asíncrono con fetch api
     fetch(beersJson)
@@ -23,6 +27,27 @@ function getBeers(){
         console.log('Llegó la birra!'); 
     })
 }
+
+
+
+function getAssets(){
+    //consumir un json de modo asíncrono con fetch api
+    fetch(assetsJson)
+    .then(response =>{
+            return response.json();//el método json() extrae el cuerpo del recurso solicitado
+    })
+    .then(data => {//data son los datos del response
+        assetsArr.push(data);
+        console.log(data)
+    })
+    .catch(error => {//captura si hubo un error en la petición
+        console.log(error);
+    })
+    .finally(()=>{//fynally nos permite hacer algo si todo salió bien
+        console.log('Llegaron los Assests!'); 
+    })
+}
+
 
 
 
